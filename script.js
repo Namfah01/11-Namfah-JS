8d733fb3b05361428ac4d322db1b17700bcd9d94
Namfah01 <kanokwanbing09@gmail.com>
2024-05-24 14:39:40 +0700

    cart

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 //เริ่มจากการประกาศตัวแปร
 const productForm = document.getElementById('productForm');
 const productDashboard = document.getElementById('productDashboard');
-
+const cart = [];
+const addToCartBtn = document.getElementById('addToCartBtn');
 
 //Create Product section
 productForm.addEventListener('submit', function (event) {
@@ -24,7 +25,7 @@ productForm.addEventListener('submit', function (event) {
 
 //add to productdashbord section
 function addProductToDashboard(product) {
-    const productElement = document.createElement('div');           //สร้างelementใหม่ซึ่งก็คือ div ที่ภายในมี checkbox ที่แสดงผลproduct name,price พร้อมทั้งกำหนดขนาดของภาพ
+    const productElement = document.createElement('div');           //สร้างelementใหม่ซึ่งก็คือ div ที่ภายในมี checkbox ที่แสดงผลproduct name,price ในรูปแบบของspan พร้อมทั้งกำหนดขนาดของภาพ
     productElement.innerHTML = `
         <input type="checkbox" class="productSelect">
         <span>${product.name}</span>
@@ -35,5 +36,25 @@ function addProductToDashboard(product) {
     productDashboard.appendChild(productElement);       //ให้แสดงผลภายใน productDashborad
 }
 
+//add to cart
+addToCartBtn.addEventListener('click', function () {       //เพิ่มeventlistening เมื่อกดปุ่มจะเลือก Product ที่ถูกเลือกใน checkbox
+    const selectedProducts = document.querySelectorAll('.productSelect:checked');
+    cart.length = 0;
+    selectedProducts.forEach(productElement => {                 //แสดงproductที่ถูกเลือกในแต่ละครั้ง
+        const name = productElement.nextElementSibling.textContent;  //แสดงข้อความในspanของproduct name
+        const price = parseFloat(productElement.nextElementSibling.nextElementSibling.textContent.slice(1));  //ดึงราคามาจากspanของราคาสินค้า ฿100 โดยตัด฿ออก เพื่อให้เหลือแต่จำนวนราคาในรูปแบบของ sting จากนั้นใช้ parseFolat แปลงเป็นตัวเลขในรูปแบบทศนิยม และเก็บไว้ในรูปแบบของ price
+        cart.push({ name, price });  //เพิ่ม name , price ของสินค้าแต่ละชิ้นลงใน cart
+    });
+    displayCart();  //เรียกใช้ฟังก์ชั่น
+});
+
+//displayCart กำหนดรูปแบบการแสดงผลproduct ใน class
+function displayCart() {
+    const cartDiv = document.getElementById('cart');      //ดึง product element จาก cart มาใส่ใน cartDiv
+    cartDiv.innerHTML = 'Cart: ';  //กำหนดข้อความเริ่มต้นที่แสดงในcartDiv
+    cart.forEach(product => {      //วนลูปเพื่อแสดงproductที่ถูกเลือกใน cart แต่ละครั้ง
+        cartDiv.innerHTML += `${product.name} (฿${product.price}), `;   //นำค่าที่ได้มาแสดงผลในรูปแบบ name ,price(฿100)
+    });
+}
 
 
